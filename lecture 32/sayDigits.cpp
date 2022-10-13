b586b4b693c34958aa6a77dd1a88e4f5ea142d62
Anindya <anindyaramtripathi@gmail.com>
2022-10-13 23:45:24 +0530

    added file

---
+++ lecture 32/sayDigits.cpp
@@ -0,0 +1,41 @@
+/*
+say digits
+input 412    
+Expected Output=> four one two 
+*/
+
+#include<iostream>
+using namespace std;
+
+void sayDigit(int n,string arr[]){
+    //base
+    if(n==0) 
+        return ;
+    
+    //processing
+    int digit =n%10;
+    n=n/10;
+    
+    //recursive call
+    sayDigit(n,arr);
+
+    cout << arr[digit]<<" ";
+
+}
+
+int main(){
+
+    string arr[10]={"zero","one","two","three"
+                    ,"four","five","six",
+                    "seven","eight","nine"};
+
+    int n;
+    cout<<"Enter number : ";
+    cin>>n;
+
+    sayDigit(n,arr);
+    cout<<endl;
+
+    return 0;
+}
+
---
+++ lecture 32/waysToReachNthStairs.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int countStairs(int n){
+    if(n<0) return 0;
+    if(n==0) return 1;
+
+    return countStairs(n-1) +countStairs(n-2);
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter no. of stairs: ";
+    cin>>n;
+    cout << countStairs(n);
+
+    return 0;
+}
