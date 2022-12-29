432b5a88e0fe64f3a7b0dfcabed413fee238e5ee
Anindya <anindyaramtripathi@gmail.com>
2022-12-29 15:01:01 +0530

    Again on path to DSA

--- lecture 31/recurstion_1.cpp
+++ lecture 31/recurstion_1.cpp
@@ -1,11 +1,16 @@
+/*When a function call itself it's known as Recursion
+    When a problem can be divided into smaller sub problem then we can use Recursion to solve it.
+    Two Things are mandatory to solve a problem by recursion i.e Base Case and Recursive Relation
+*/
+
 #include<iostream>
 using namespace std;
 
 int factorial(int n){
-    if(n==0)
+    if(n==0) //Base case - When we need to stop the program. 
+    //If we don't make a base case then memory overflow occur known as segmentation fault.
      return 1;
-
-    return n * factorial(n-1);
+    return n * factorial(n-1); //Recursive Relation between sub problems
 
 }
 
