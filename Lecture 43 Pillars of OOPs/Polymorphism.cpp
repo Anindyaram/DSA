86faa5544121919ab1a85bb731331ee1a1b37308
Anindya <anindyaramtripathi@gmail.com>
2023-01-08 20:09:54 +0530

    Polymorphism

---
+++ Lecture 43 Pillars of OOPs/Polymorphism.cpp
@@ -0,0 +1,30 @@
+/*
+Polymorphism - single thing with mutiple forms;
+Types of Polymorphism - 
+    1> Compile Time Polymorphism
+        Types of CTP-
+            1> Funtion Overloading
+            2> Operator Overloading
+    2> Run Time Polymorphism;
+*/
+#include<iostream>
+using namespace std;
+
+class poly
+{
+public:
+    void he(){
+        cout<<"HEHEHEHEHEHEHEHEH"<<endl;
+    }
+    void he(string a){
+        cout<<a<<" HAHAHAHAHAHHAHAHA!!";
+    }
+};
+
+int main(){
+    poly be;
+    be.he();
+    be.he("Women");
+
+    return 0;
+}
